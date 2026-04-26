2847f9d123c6dfa0c4899284f24efc1ced302876
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2026-04-26 15:59:41 +0300

    Fix Loan Approval

--- feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanApproval/LoanAccountApprovalViewModel.kt
+++ feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanApproval/LoanAccountApprovalViewModel.kt
@@ -9,24 +9,25 @@
  */
 package com.mifos.feature.loan.loanApproval
 
-import androidclient.feature.loan.generated.resources.Res
-import androidclient.feature.loan.generated.resources.feature_loan_unknown_error_occured
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import co.touchlab.kermit.Logger
 import com.mifos.core.common.utils.DataState
 import com.mifos.core.data.repository.LoanAccountApprovalRepository
 import com.mifos.room.entities.accounts.loans.LoanApprovalData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
-import org.jetbrains.compose.resources.getString
 
 class LoanAccountApprovalViewModel(
     private val repository: LoanAccountApprovalRepository,
     savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
+
+    private val log = Logger.withTag(TAG)
 
     private val arg = savedStateHandle.getStateFlow(key = "arg", initialValue = "")
     private val loanAccountData: LoanApprovalData = Json.decodeFromString<LoanApprovalData>(arg.value)
@@ -39,28 +40,54 @@ class LoanAccountApprovalViewModel(
     var loanWithAssociations = loanAccountData.loanWithAssociations
 
     fun approveLoan(loanApproval: com.mifos.core.model.objects.account.loan.LoanApproval?) {
+        log.i {
+            "approveLoan() called: loanId=$loanId, " +
+                "approvedOnDate=${loanApproval?.approvedOnDate}, " +
+                "approvedLoanAmount=${loanApproval?.approvedLoanAmount}, " +
+                "expectedDisbursementDate=${loanApproval?.expectedDisbursementDate}, " +
+                "note=${loanApproval?.note}, " +
+                "locale=${loanApproval?.locale}, " +
+                "dateFormat=${loanApproval?.dateFormat}"
+        }
+
         viewModelScope.launch {
-            repository.approveLoan(loanId, loanApproval).collect { dataState ->
-                when (dataState) {
-                    is DataState.Loading -> {
-                        _loanAccountApprovalUiState.value =
-                            LoanAccountApprovalUiState.ShowProgressbar
-                    }
+            repository.approveLoan(loanId, loanApproval)
+                .catch { throwable ->
+                    log.e(throwable) { "approveLoan() flow threw: ${throwable.message}" }
+                    _loanAccountApprovalUiState.value =
+                        LoanAccountApprovalUiState.ShowLoanApproveFailed(
+                            throwable.message ?: "Unknown error",
+                        )
+                }
+                .collect { dataState ->
+                    when (dataState) {
+                        is DataState.Loading -> {
+                            log.d { "approveLoan() loading…" }
+                            _loanAccountApprovalUiState.value =
+                                LoanAccountApprovalUiState.ShowProgressbar
+                        }
 
-                    is DataState.Success -> {
-                        val response = dataState.data
-                        _loanAccountApprovalUiState.value =
-                            LoanAccountApprovalUiState.ShowLoanApproveSuccessfully(response)
-                    }
+                        is DataState.Success -> {
+                            log.i { "approveLoan() success: response=${dataState.data}" }
+                            _loanAccountApprovalUiState.value =
+                                LoanAccountApprovalUiState.ShowLoanApproveSuccessfully(dataState.data)
+                        }
 
-                    is DataState.Error -> {
-                        _loanAccountApprovalUiState.value =
-                            LoanAccountApprovalUiState.ShowLoanApproveFailed(
-                                getString(Res.string.feature_loan_unknown_error_occured),
-                            )
+                        is DataState.Error -> {
+                            log.e(dataState.exception) {
+                                "approveLoan() failed: ${dataState.message}"
+                            }
+                            _loanAccountApprovalUiState.value =
+                                LoanAccountApprovalUiState.ShowLoanApproveFailed(
+                                    dataState.message,
+                                )
+                        }
                     }
                 }
-            }
         }
+    }
+
+    private companion object {
+        const val TAG = "LoanApproval"
     }
 }
